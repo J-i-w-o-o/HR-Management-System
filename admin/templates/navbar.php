3604e09368d622006d97289a22787c19f308c351
Jean Patrice Molina <molinajeanpatrice.bsit@gmail.com>
2023-07-05 17:02:10 +0800

    LINE

--- admin/templates/navbar.php
+++ admin/templates/navbar.php
@@ -74,11 +74,11 @@
   <div id="mySidenav" class="sidenav">
 
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <a class="border-bottom border-white border-2" href="index.php?dashboard" data-toggle="tooltip" data-placement="bottom" title="DASHBOARD"><i class="fa fa-home mx-2"> </i>DASHBOARD</a>
-    <a href="index.php?job-listing" data-toggle="tooltip" data-placement="bottom" title="JOB LISTING"><i class="fa fa-user mx-2"> </i>JOB LISTING</a>
+    <a class="border-bottom border-white border-2 mx-2" href="index.php?dashboard" data-toggle="tooltip" data-placement="bottom" title="DASHBOARD"><i class="fa fa-home mx-2"> </i>DASHBOARD</a>
+    <a class="border-bottom border-white border-2 mx-2" href="index.php?job-listing" data-toggle="tooltip" data-placement="bottom" title="JOB LISTING"><i class="fa fa-user mx-2"> </i>JOB LISTING</a>
     <a href="index.php?job-application" data-toggle="tooltip" data-placement="bottom" title="JOB APPLICATION"><i class="fa fa-user mx-2"> </i>JOB APPLICATION</a>
     <div class="sidenav-footer">
-        <a href="index.php?logout" data-toggle="tooltip" data-placement="bottom" title="LOGOUT"><i class="fa fa-sign-out-alt mx-2"></i>LOGOUT</a>
+        <a class="border-bottom border-white border-2" href="index.php?logout" data-toggle="tooltip" data-placement="bottom" title="LOGOUT"><i class="fa fa-sign-out-alt mx-2"></i>LOGOUT</a>
   </div>
   </div>
   <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
