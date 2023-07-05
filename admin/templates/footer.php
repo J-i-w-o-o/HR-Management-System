c6767d3601ba5d6f6267dfc2a6d6d5ec77828c9d
alexislanuza <alexislanuza06@gmail.com>
2023-07-05 11:40:11 +0800

    careers update

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -3,6 +3,5 @@
 <script src="../assets/css/node_modules/mdb-ui-kit/js/mdb.min.js"></script>
 <script src="../assets/js/node_modules/jquery/dist/jquery.min.js"></script>
 <script src="../assets/css/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="path/to/jquery.min.js"></script>
-<script src="path/to/datatables.min.js"></script>
+
 </html>
