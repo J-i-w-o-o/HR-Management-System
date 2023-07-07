e37082d4eb6abca8f18904a9bb2d1f2cf96bc121
Jiwoo <andrielgeomergabriel@gmail.com>
2023-07-07 16:07:07 +0800

    added

---
+++ admin/job-application-accepted.php
@@ -0,0 +1,34 @@
+<title>Job Application</title>
+<?php include 'C:\xampp\htdocs\HR-Management-System\assets\css\job-application.css';?>
+
+<div id="main">
+<section id="hero" class="pt-5 mt-8 align-items-center">
+      <h2 class="mt-2 ms-3"> PENDING APPLICATION</h2>
+      
+
+      <div id="table-scroll" class="table-scroll">
+  <div class="table-wrap">
+    <table class="main-table">
+  <div id="tableres">
+
+      <div id="tableres" style="overflow-x:auto;">
+        <table>
+          <div>
+            <tr>
+              <th>Position</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Mobile</th>
+              <th>Email</th>
+              <th>Resume</th>
+              <th>Action</th>
+              <th>Status</th>
+
+            </tr>
+          </div>
+          <?php 
+          include 'C:\xampp\htdocs\HR-Management-System\includes\job-applicationconf.php';?>
+        </table>
+      </div>
+      
+
