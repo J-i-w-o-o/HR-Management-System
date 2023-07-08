7d11f31789893dc79028c667ac783b5029a12ba7
Jean Patrice Molina <molinajeanpatrice.bsit@gmail.com>
2023-07-08 11:02:56 +0800

    dropdown

--- admin/templates/navbar.php
+++ admin/templates/navbar.php
@@ -11,14 +11,14 @@
     <div class="dropdown">
   <a href="#" class="dropdown-toggle" onclick="toggleDropdown()"> <i class="fa fa-file mx-2"> </i> JOB APPLICATION</a>
   <div class="dropdown-content" >
-    <a href="index.php?job-application">Pending Application</a>
-    <a href="index.php?job-application-accepted">Approved Application</a>
-    <a href="index.php?job-application-declined">Declined Application</a>
+    <a href="index.php?job-application"><i class="fa fa-clock mx-2"> </i>Pending Application</a>
+    <a href="index.php?job-application-accepted"><i class="fa fa-user-plus mx-2"> </i>Approved Application</a>
+    <a href="index.php?job-application-declined"><i class="fa fa-user-times mx-2"> </i>Declined Application</a>
   </div>
 </div>
     
     <div class="sidenav-footer">
-        <a class="border-bottom border-white border-2" href="index.php?logout" data-toggle="tooltip" data-placement="bottom" title="LOGOUT"><i class="fa fa-sign-out-alt mx-2"></i>LOGOUT</a>
+        <a class="border-bottom border-black border-2" href="index.php?logout" data-toggle="tooltip" data-placement="bottom" title="LOGOUT"><i class="fa fa-sign-out-alt mx-2"></i>LOGOUT</a>
   </div>
   
   </div>
