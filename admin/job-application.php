d74bfb6308912294a2e5b35c4e36deff5bf99ddd
Jiwoo <andrielgeomergabriel@gmail.com>
2023-07-10 10:20:54 +0800

    asdad

--- admin/job-application.php
+++ admin/job-application.php
@@ -2,17 +2,6 @@
 <?php include 'C:\xampp\htdocs\HR-Management-System\assets\css\job-application.css'; ?>
 
 <div id="main">
-<section id="hero" class="pt-5 mt-8 align-items-center">
-
-  <!-- <ol class="breadcrumb">
-    <li class="breadcrumb-item "><a href="index.php?job-application">Pending Application</a></li>
-    <li class="breadcrumb-item active" aria-current="page" ><a href="index.php?job-application-accepted">Approved Applicants</a></li>
-    <li class="breadcrumb-item active" aria-current="page"><a href="index.php?job-application-declined">Declined Applicants</a></li>
-    <li class="breadcrumb-item active" aria-current="page"><a href="index.php?job-application-interview">For Interview</a></li>
-  </ol> -->
-
-      <h2 class="mt-2 ms-3"> PENDING APPLICATIONS</h2>
-      
   <section id="hero" class="pt-5 mt-8 align-items-center">
     <h2 class="mt-2 ms-3"> PENDING APPLICATIONS</h2>
     <div id="table-scroll" class="table-scroll">
@@ -32,7 +21,6 @@
                     <th>Resume</th>
                     <th>Action</th>
                     <th>Status</th>
-
                   </tr>
                 </div>
                 <?php
