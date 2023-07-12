494f7769d1dc233c4a1af772c70c2d53f052f2fd
alexislanuza <alexislanuza06@gmail.com>
2023-07-12 09:59:28 +0800

    employees card (backend na lang kulang)

--- admin/employee.php
+++ admin/employee.php
@@ -1,81 +1,74 @@
 <title>Employee</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-
-<!-- Add the Bootstrap JS library link -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
-<!-- Add your custom JavaScript function -->
-
 <link rel="stylesheet" href="../assets/css/employee.css">
-<style>
-  #image-display {
-    width: 200px; /* Set your desired width */
-    height: 200px; /* Set your desired height */
-    overflow: hidden;
-  }
-
-  #image-display img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-</style>
-
 <div id="main">
 
+  <!-- title page and search bar  -->
   <div class="d-flex justify-content-between align-items-center mx-2">
     <div>
-      <h4 class="ms-3 my-3"><span id="jobCount"></span>Employees</h4>
+      <h4 class="text-center lead ms-3 mt-2 panel-primary"><span id="jobCount"></span>(put here no. of employee)</h4>
     </div>
-    <div class="d-flex">
-      <form class="d-flex forms my-3">
-        <input type="text" id="searchInput" class="form-control form-control-sm me-2" placeholder="Search Employee">
-      </form>
-    </div>
+    <form class="d-flex forms my-3">
+      <input type="text" id="searchInput" class="form-control form-control-sm me-2" placeholder="Search for Employee">
+    </form>
   </div>
 
-  <div class="row ms-2 me-2">
-    <div class="column">
-      <div class="card">
-        <div>
-          <div class="float-right">
-            <button class="editbtn no-border"><i class="fa-solid fa-ellipsis"></i></button>
-          </div>
-        </div>
+  <!-- card content -->
+  <div class="text-center">
+  </div>
+  <div class="container-fluid">
+    <div id="jobsContainer" class="row">
+      <div class="col-md-4 mb-3 d-flex">
+
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="DITO MAD-DISPLAY YUNG PROFILE NG EMPLOYEE">
+
           <div class="card-body">
-            <div class="border border-dark" id="image-display">
-              
+            <h5 class="card-title text-muted">Position/Department</h5>
+            <p class="card-text">Name:</p>
+            <p class="card-text">Contact:</p>
+            <p class="card-text">Date Hired:</p>
+            <p class="card-text"></p>
+          </div>
+
+          <div class="card-body text-muted d-flex justify-content-between align-items-center">
+            <button type="button" class="btn rounded-pill" data-bs-toggle="modal" data-bs-target="#jobModal"><i class="fa-solid fa-user-pen" style="color: #ec5b33;"></i></button>
+          </div>
+
+        </div>
+      </div>
+
+      <!-- modal start here -->
+      <div class="modal fade" id="jobModal" tabindex="-1" aria-labelledby="jobLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content custom-scrollbar">
+            
+          <div class="modal-header">
+              <h1 class="modal-title fs-5" id="jobLabel"></h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <h5 class="mt-2">Position</h5>
-            <p>Name</p>
-            <p>Contact</p>
-            <p>Date Hired</p>
+
+            <div class="modal-body">
+              <h2>Edit Profile</h2>
+              <p id="overview"></p>
+              <div class="row">
+                <div class="col-6">
+                  <p class="card-text">Name:</p>
+                  <p class="card-text">Contact:</p>
+                  <p class="card-text">Date Hired:</p>
+                  <p class="card-text">Resume:</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="modal-footer">
+              <button type="button" id="btnclose" class="btn" data-bs-dismiss="modal"><i class="fa-solid fa-trash"></i></button>
+              <button type="submit" id="btnappsubmit" class="btn " form="applicationform"><i class="fa-solid fa-pen"></i></button>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
+
   </div>
 </div>
-<script>
-  function uploadImage() {
-    const fileInput = document.getElementById('image-upload');
-    const imageDisplay = document.getElementById('image-display');
-
-    if (fileInput.files && fileInput.files[0]) {
-      const reader = new FileReader();
-
-      reader.onload = function (e) {
-        const img = document.createElement('img');
-        img.src = e.target.result;
-
-        // Clear previous image (if any)
-        while (imageDisplay.firstChild) {
-          imageDisplay.removeChild(imageDisplay.firstChild);
-        }
-
-        imageDisplay.appendChild(img);
-      };
-
-      reader.readAsDataURL(fileInput.files[0]);
-    }
-  }
-</script>
