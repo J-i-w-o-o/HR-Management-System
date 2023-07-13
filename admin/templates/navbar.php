3dbdc5675370c60b27300100141a8789ea830ce5
Jean Patrice Molina <molinajeanpatrice.bsit@gmail.com>
2023-07-13 11:08:20 +0800

    inayos lang

--- admin/templates/navbar.php
+++ admin/templates/navbar.php
@@ -1,6 +1,6 @@
 <header>
 
-  <div id="mySidenav" class="sidenav shadow rounded ">
+<div id="mySidenav" class="sidenav shadow rounded ">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     <a class="border-bottom border-black border-2 mx-2" href="index.php?dashboard" data-toggle="tooltip" data-placement="bottom" title="DASHBOARD"><i class="fa fa-tachometer mx-2"> </i>Dashboard</a>
     <a class="border-bottom border-black border-2 mx-2" href="index.php?job-listing" data-toggle="tooltip" data-placement="bottom" title="JOB LISTING"><i class="fa fa-list mx-2"> </i>Job Listing</a>
