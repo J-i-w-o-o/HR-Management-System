253ac008dbecdcd5f04267b04d9dc51b124ddba7
Jean Patrice Molina <molinajeanpatrice.bsit@gmail.com>
2023-07-31 11:09:58 +0800

    searchbar

--- admin/job-application-declined.php
+++ admin/job-application-declined.php
@@ -6,8 +6,8 @@
   <section id="hero" class=" align-items-center">
     <div class="d-flex justify-content-between align-items-center mx-2">
       <h2 class="mt-2 ms-3"> DECLINED APPLICATIONS</h2>
-      <form class="d-flex forms my-3">
-        <input type="text" id="searchInput" class="form-control form-control-sm me-2" placeholder="Search for Applicant">
+      <form class="d-flex forms my-2 mx-3">
+        <input type="text" id="searchInput" class="form-control form-control-sm me-2" placeholder="Search">
         <button type="submit" class="button btn-sm" id="searchIcon">
               <i class="fas fa-search"></i>
             </button>
