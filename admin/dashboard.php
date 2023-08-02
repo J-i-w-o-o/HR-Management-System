f9815582badb429858ba88c5b67aeecbfec2124a
Jean Patrice Molina <molinajeanpatrice.bsit@gmail.com>
2023-08-02 13:27:28 +0800

    dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -19,7 +19,7 @@ $totalEmployees = $row['totalEmployees'];
 ?>
 
 <title>Dashboard</title>
-
+<link href="https://fonts.googleapis.com/css?family=Waiting+for+the+Sunrise" rel="stylesheet" type="text/css"/>
   <link rel="stylesheet" href="../assets/css/node_modules/dashboard.css">
   <div class="container-fluid px-4">
     <section id="hero" class="align-items-center">
@@ -35,7 +35,9 @@ $totalEmployees = $row['totalEmployees'];
           </div>
         </div>
       </div>
-      <h2 class="mt-2 ms-3"> DASHBOARD</h2>
+      
+      <div id="typedtext"></div>
+      
       <div class="row">
         <div class="col-xl-3">
           <div class="card mb-3">
@@ -81,3 +83,40 @@ $totalEmployees = $row['totalEmployees'];
     </section>
   </div>
 </div>
+<script>// set up text to print, each item in array is new line
+var aText = new Array(
+"WELCOME TO DASHBOARD, ADMIN!"
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+
+typewriter();</script>
